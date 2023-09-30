c3d9a7ea42938c339306839a63acd84f9c783c5a
Juan Cruz <j.cruzb@outlook.com>
2023-09-30 19:29:14 +0200

    fixed key error

--- src/views/Products/ProductsList.jsx
+++ src/views/Products/ProductsList.jsx
@@ -54,7 +54,7 @@ const Product = () => {
       </Button>
       <Grid container spacing={2}>
         {products.map((product) => (
-          <Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
             <ProductCard
               product={product}
               onDelete={handleDeleteProduct}
