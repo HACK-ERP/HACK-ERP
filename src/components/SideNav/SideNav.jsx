7741812b62b0d3e297aa3508ac12d73d579a225b
Juan Cruz <j.cruzb@outlook.com>
2023-10-14 10:55:38 +0200

    update

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,15 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import ArrowTopRightOnSquareIcon from '@heroicons/react/24/solid/ArrowTopRightOnSquareIcon';
 import {
   Box,
-  Button,
   Divider,
   Drawer,
   Link,
   Stack,
-  SvgIcon,
-  Typography,
   useMediaQuery
 } from '@mui/material';
 
@@ -19,7 +15,7 @@ import { SideNavItem } from './side-nav-item';
 
 export const SideNav = (props) => {
   const { open, onClose } = props;
-  const  pathname  = useNavigate();
+  const pathname = useNavigate();
   const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'));
 
   const content = (
@@ -41,7 +37,7 @@ export const SideNav = (props) => {
           height: '100%'
         }}
       >
-        
+
         <Box sx={{ p: 3 }}>
           <Box
             sx={{
@@ -100,56 +96,7 @@ export const SideNav = (props) => {
           </Stack>
         </Box>
         <Divider sx={{ borderColor: 'white' }} />
-        <Box
-          sx={{
-            px: 2,
-            py: 3
-          }}
-        >
-          <Typography
-            color="white"
-            variant="subtitle2"
-          >
-            Need more features?
-          </Typography>
-          <Typography
-            color="white"
-            variant="body2"
-          >
-            Check out our Pro solution template.
-          </Typography>
-          <Box
-            sx={{
-              display: 'flex',
-              mt: 2,
-              mx: 'auto',
-              width: '160px',
-              '& img': {
-                width: '100%'
-              }
-            }}
-          >
-            <img
-              alt="Go to pro"
-              src="/assets/devias-kit-pro.png"
-            />
-          </Box>
-          <Button
-            component="a"
-            endIcon={(
-              <SvgIcon fontSize="small">
-                <ArrowTopRightOnSquareIcon />
-              </SvgIcon>
-            )}
-            fullWidth
-            href="https://material-kit-pro-react.devias.io/"
-            sx={{ mt: 2 }}
-            target="_blank"
-            variant="contained"
-          >
-            Pro Live Preview
-          </Button>
-        </Box>
+
       </Box>
     </Box>
   );
